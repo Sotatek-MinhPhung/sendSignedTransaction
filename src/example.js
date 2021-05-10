60af8cada07a14744277827437c82c720b40530e
Sotatek-MinhPhung <81953164+Sotatek-MinhPhung@users.noreply.github.com>
2021-05-10 17:27:29 +0700

    update

--- src/example.js
+++ src/example.js
@@ -3,9 +3,10 @@ const Web3 = require('web3')
 const MyContract = require("../src/abis/MyContract.json")
 const rinkeby_infura_url = "https://rinkeby.infura.io/v3/e180608a18e74c5397b40e760f7a31f7"
 
-
-const address = "0xA853531E6bd0129c9389738E8F00603619e15fE2"
-const privateKey = "36d712be341d6564d24e3844e698c4269eb8302fafadcb13957dbe249e1737d5"
+// account adress
+const address = ""
+// account privatekey
+const privateKey = ""
 
 const init = async () => {
     const web3 = new Web3(rinkeby_infura_url);
