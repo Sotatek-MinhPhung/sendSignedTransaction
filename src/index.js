d0e0fde37ea75da50078e4b075ff3e089a76276b
Sotatek-MinhPhung <81953164+Sotatek-MinhPhung@users.noreply.github.com>
2021-05-10 17:27:51 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ const Web3 = require('web3')
 const rinkeby_infura_url = "https://rinkeby.infura.io/v3/e180608a18e74c5397b40e760f7a31f7"
 
 const account1 = {
-    ADDRESS: "0xA853531E6bd0129c9389738E8F00603619e15fE2",
-    PRIVATE_KEY: "36d712be341d6564d24e3844e698c4269eb8302fafadcb13957dbe249e1737d5"
+    ADDRESS: "",
+    PRIVATE_KEY: ""
 }
 const account2 = {
-    ADDRESS: "0x1002271d02E8a72aA642855A56394791fb3cb46D",
-    PRIVATE_KEY: "4bbacdc2bf6caba90e22399f0f6c77d876727c7999e98b3b9031a8a1b6ffab1b"
+    ADDRESS: "",
+    PRIVATE_KEY: ""
 }
 
 const sendEth = async (address, privateKey, address2, value) => {
